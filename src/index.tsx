35d2b6b489a14b87bbe7de49b6e1c7d5ef1abeef
KoseiAoyagi <45550286+azul915@users.noreply.github.com>
2020-01-24 08:40:44 +0900

    Add judge system

--- src/index.tsx
+++ src/index.tsx
@@ -1,35 +1,25 @@
+/* 参考先　https://note.com/tkugimot/n/nf7fe751298b1 */
 import React from "react";
 import ReactDOM from "react-dom";
 import "./styles.css";
 
 // TypeScriptはプロパティの定義にInterfaceが必要
 interface SquarePropsInterface {
-  value: number;
-}
-
-interface SuqareStateInterface {
   value: string;
+  onClick: () => void;
 }
 
-class Square extends React.Component<
-  SquarePropsInterface,
-  SuqareStateInterface
-> {
-  constructor(props: SquarePropsInterface) {
-    super(props);
-    this.state = {
-      value: ""
-    };
-  }
-  render() {
-    return (
-      <button className="square" onClick={() => this.setState({ value: "X" })}>
-        {this.state.value}
-      </button>
-    );
-  }
-}
-
+// 関数コンポーネントは、render メソッドだけを有して自分の state を持たないコンポーネントを、よりシンプルに書くための方法
+const Square = (props: SquarePropsInterface) => {
+  return (
+    // 関数コンポーネントは引数からpropsを受け取るのでthisが要らない
+    // onClick={props.onClick}でもよい
+    // buttonタグのonClick属性は特別な意味を持つ
+    <button className="square" onClick={() => props.onClick()}>
+      {props.value}
+    </button>
+  );
+};
 interface BoardPropsInterface {
   squares: Array<string>;
 }
@@ -39,19 +29,44 @@ interface BoardStateInterface {
 }
 
 class Board extends React.Component<BoardPropsInterface, BoardStateInterface> {
+  /* JavaScript のクラスでは、サブクラスのコンストラクタを定義する際は常に super を呼ぶ必要があります。constructor を持つ React のクラスコンポーネントでは、すべてコンストラクタを super(props) の呼び出しから始めるべきです。 */
   constructor(props: BoardPropsInterface) {
     super(props);
     this.state = {
-      squares: Array(9).fill("")
+      squares: Array(9).fill(""),
+      xIsNext: true
     };
   }
 
+  handleClick(i: number) {
+    // 引数なしのslice()で別のオブジェクトを作り、immutableにする
+    const squares: Array<string> = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? "X" : "O";
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext
+    });
+  }
+
   renderSquare(i: number) {
-    return <Square value={i} />;
+    return (
+      // コンポーネントタグのonClickはReactにおける予約語ではない
+      // イベントを表すpropsにはon[Event]、イベントを処理するメソッドにはhandle[Event]が名付けられる慣習
+      <Square
+        value={this.state.squares[i]}
+        onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    const status = "Next player: X";
+    const winner = calculateWinner(this.state.squares);
+    let status;
+    if (winner === "O" || winner === "X") {
+      status = `Winner: ${winner}`;
+    } else {
+      status = `Next player: ${this.state.xIsNext ? "X" : "O"}`;
+    }
 
     return (
       <div>
@@ -90,6 +105,28 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+// 勝敗判定
+// 9 つの square の配列が与えられると、この関数は勝者がいるか適切に確認し、'X' か 'O'、あるいは null を返します。
+function calculateWinner(squares: Array<string>): string {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return "";
 }
 
 // ========================================
